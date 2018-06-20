0108f3eda99b64e52e5cadd2e71180554e6d4753
PetrosM93 <40371032+PetrosM93@users.noreply.github.com>
2018-06-20 20:03:47 +0300

    Fix margins Simerines Paraggelies ( MarginBottom -> MarginTop ) New date New Constrain layout @ main

--- app/src/main/java/com/example/terrorizer/teopiotrpromitheuths/MainActivity.java
+++ app/src/main/java/com/example/terrorizer/teopiotrpromitheuths/MainActivity.java
@@ -1,15 +1,15 @@
 package com.example.terrorizer.teopiotrpromitheuths;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.text.DateFormat;
-import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -47,11 +47,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Calendar calendar = Calendar.getInstance();  //gia thn hmerominia
-        String currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
-
+        Date date = new Date();
+        Locale locale = new Locale("el", "GR");
+        String dateprint = new SimpleDateFormat("EEEE dd-MM-yyy",locale).format(date);
         TextView textViewDate = findViewById(R.id.date);
-        textViewDate.setText(currentDate);
+        textViewDate.setText(dateprint);
     }
 
     public void openOrder(){  //kaleitai mesw toy onClickListener
